a832c8a0f6ef50914ef67f7c9d0e75144d2b0423
Domenico-pio <domenicopioferrara@gmail.com>
2021-12-31 11:31:33 +0100

    update

---
+++ js/quiz.js
@@ -0,0 +1,50 @@
+function check() {
+  var risp=document.getElementById("risposta");
+  if(document.getElementById("correct").checked){
+
+    document.getElementById("risultato").innerHTML="risposta correta";
+    console.log(risp.firstChild.innerHTML);
+  }
+  else {
+    document.getElementById("risultato").innerHTML="risposta sbagliata";
+    console.log(risp.firstChild.innerHTML);
+  };
+  risp.style.display="";
+}
+
+var parametersQuiz = {
+"id":"applet2",
+"scaleContainerClass": 'graphs',
+"showMenuBar":false,
+"showAlgebraInput":false,
+"showToolBar":false,
+"showResetIcon":false,
+"showToolBarHelp":false,
+"enableLabelDrags":false,
+"enableShiftDragZoom":false,
+"enableRightClick":false,
+"errorDialogsActive":false,
+"useBrowserForJS":false,
+"allowStyleBar":false,
+"preventFocus":true,
+"showZoomButtons":false,
+"capturingThreshold":3,
+//"appletOnLoad":function(api){ api.setValue("applet",0); api.setValue("applet2",1); api.setValue("applet3",0); api.setValue("applet4",0); api.setValue("applet5",0);  },
+"showFullscreenButton":false,
+"scale":1,
+"disableAutoScale":false,
+"allowUpscale":false,
+"clickToLoad":false,
+"appName":"classic",
+"buttonRounding":0.7,
+"buttonShadows":false,
+"language":"it",
+"filename":"geo/quiz.ggb",
+
+};
+var views = {'is3D': 0,'AV': 0,'SV': 0,'CV': 0,'EV2': 0,'CP': 0,'PC': 0,'DA': 0,'FI': 0,'macro': 0};
+var appquiz= new GGBApplet(parametersQuiz,views);
+window.onload=function () {
+  appquiz.setPreviewImage('data:image/gif;base64,R0lGODlhAQABAAAAADs=','https://www.geogebra.org/images/GeoGebra_loading.png','https://www.geogebra.org/images/applet_play.png');
+  appquiz.inject('ggbquiz');
+}
